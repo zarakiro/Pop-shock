ecb5a9e87fd747b45dedc56ae03ed22fd9f76907
Matt <147386363+zarakiro@users.noreply.github.com>
2024-03-26 10:14:24 +0100

    Update 4piezo_accelero_beetle_sans_wifi.ino

--- 4piezo_accelero_beetle_sans_wifi.ino
+++ 4piezo_accelero_beetle_sans_wifi.ino
@@ -6,15 +6,15 @@ Adafruit_MPU6050 mpu;
 
 const char *ssid = "ESP32_Server"; // Nom du réseau Wi-Fi créé par l'ESP32C3
 const char *password = "password"; // Mot de passe du réseau Wi-Fi
-const int sensor1=1;//piezo gauche
-const int sensor2=2;//piezo droite
-const int sensor3=4;//piezo devant
-const int sensor4=3;// a changer parce que adc2 pas pris encompte mais piezzo au dessus
+const int sensor1=A1;//piezo gauche
+const int sensor2=A2;//piezo droite
+const int sensor3=A4;//piezo devant
+const int sensor4=A3;// a changer parce que adc2 pas pris encompte mais piezzo au dessus
 
 WiFiServer server(12345); // Création d'un serveur TCP sur le port 12345
 
 void setup() {
-  Serial.begin(921600);
+  Serial.begin(115200);
   Serial.println("SETUP");
   Serial.println("Adafruit MPU6050 test!");
   if (!mpu.begin()) {
@@ -41,6 +41,12 @@ void setup() {
 }
 
 void loop() {
+  WiFiClient client = server.available(); // Vérification de la connexion client
+
+  if (client) {
+    Serial.println("Nouveau client connecté");
+    
+    while (client.connected()) {
       // calcul données accelero
       sensors_event_t a, g, temp;
       mpu.getEvent(&a, &g, &temp);
@@ -51,17 +57,29 @@ void loop() {
       piezoValues[1] = analogRead(sensor2);
       piezoValues[2] = analogRead(sensor3);
       piezoValues[3] = analogRead(sensor4);
-      
+
+      //Envoi wifi
+      client.print(abs(AccTotal));
+      client.print(",");   
+      for (int i = 0; i < 4; i++) {
+        client.print(abs(piezoValues[i]));
+        client.print(",");
+      }      
       //Print en serial pour vérifier
       Serial.print(AccTotal);
       Serial.print(",");
-      Serial.print(piezoValues[0]);
+      Serial.print(abs(piezoValues[0]));
       Serial.print(",");
-      Serial.print(piezoValues[1]);
+      Serial.print(abs(piezoValues[1]));
       Serial.print(",");
-      Serial.print(piezoValues[2]);
-      Serial.print(",");
-      Serial.println(piezoValues[3]);
+      Serial.print(abs(piezoValues[2]));
+      Serial.print(",");	
+      Serial.println(abs(piezoValues[3]));
 
-      delay(200); // Délai d'attente entre chaque envoi de données
+      delay(100); // Délai d'attente entre chaque envoi de données
+    }
+
+    client.stop(); // Déconnexion du client
+    Serial.println("Client déconnecté");
   }
+}
